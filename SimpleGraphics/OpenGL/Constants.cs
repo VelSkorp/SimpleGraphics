9248c520245a3f10a65d305dece53503d2ecf02d
VelSkorp <ukantsevich@gmail.com>
2024-04-10 17:18:56 +0200

    Added OpenGL simple graphics

---
+++ SimpleGraphics/OpenGL/Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace OpenGL
+{
+	public static class Constants
+	{
+		public const string TITLE = "Simple Window";
+		public const int WIDTH = 1280;
+		public const int HEIGHT = 720;
+		public static readonly float[] Vertices = [
+				-1.0f, -1.0f,
+				 1.0f, -1.0f,
+				 1.0f,  1.0f,
+				 1.0f,  1.0f,
+				-1.0f,  1.0f,
+				-1.0f, -1.0f,
+			];
+	}
+}
---
+++ SimpleGraphics/OpenGL/Shaders/Vertex.shader
@@ -0,0 +1,8 @@
+#version 460 core
+
+in vec2 positions;
+
+void main()
+{
+	gl_Position = vec4(positions.xy, 0.0, 1.0);
+}
